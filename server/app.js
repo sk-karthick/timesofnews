e67b70839b0e460e66cc136bf0b504dba45458b8
sk-karthick <skarthicksvpr@gmail.com>
2025-06-14 22:37:03 +0530

    Add cors for dev mode.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 import express from "express";
+import cors from 'cors';
+
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/api', (req, res) => {
+app.use(express.json());
+
+app.use(cors({
+    origin: '*', // or use '*' for development
+    credentials: true // if you're using cookies or sessions
+}));
+
+
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
