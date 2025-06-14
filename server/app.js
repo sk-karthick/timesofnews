17841707cda24783b3bcec6ea1057acd04f8baa8
Karthick_S <162708547+sk-karthick@users.noreply.github.com>
2025-06-14 21:55:23 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
 
